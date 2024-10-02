e15275d9515130b7501c7fad2b281c44c6d2d0de
deemon1988 <dmn72835@gmail.com>
2024-10-02 15:29:43 +0300

    Практическое задание по уроку "Строки и индексация строк"

---
+++ hw_strings.py
@@ -0,0 +1,13 @@
+# Практическое задание по уроку "Строки и индексация строк"
+
+example = "University"
+print(example[0])
+print(example[-1])
+print(example[5:10])
+print(example[::-1])
+print(example[1::2])
+
+
+
+
+
---
+++ strings.py
@@ -0,0 +1,6 @@
+# Строки и Индексация строк
+
+name = "Dmitriy"
+print(name[-1])
+print(name[0:4:3])
+print(name[::-1])
